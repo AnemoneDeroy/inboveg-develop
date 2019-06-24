1eb6676b2b1c85134209589655e9b40b512af896
ElsDeBie <els.debie@inbo.be>
2019-06-24 11:26:48 +0200

    verder aanpassen Name --> SurveyName, schrijffouten, afwerken

--- src/utils/read_iv_header_info_edb.R
+++ src/utils/read_iv_header_info_edb.R
@@ -3,9 +3,9 @@
 #' @description This function queries the INBOVEG database for Header information (metadata for a vegetation-relevé) for one survey by the name of the survey and the recorder type. See the examples for how to get information for all surveys.
 #'
 #' @param SurveyName A character vector giving the name of the survey for which
-#' you want to extraxt header information.
+#' you want to extract header information.
 #' @param RecType A character vector giving the name of record type for which
-#' you want to extraxt header information e.g. 'Classic', 'Classic-emmer', 'Classic-ketting', 'BioHab', 'ABS'.
+#' you want to extract header information e.g. 'Classic', 'Classic-emmer', 'Classic-ketting', 'BioHab', 'ABS'.
 #' @param con dbconnection with the database 'Cydonia' on the inbo-sql07-prd server
 #'
 #' @return A data.frame with variables RecordingGivid, Name, UserReference, LocationCode, Latitude, Longitude, Area, Length, Width, SurveyId, RecTypeID.
@@ -16,16 +16,16 @@
 #'
 #' @examples
 #' con <- DBI::dbConnect(odbc::odbc(), .connection_string = "Driver=SQL Server;Server=inbo-sql07-prd.inbo.be,1433;Database=D0010_00_Cydonia;Trusted_Connection=Yes;")
-#' Name <- "OudeLanden_1979"
+#' SurveyName <- "OudeLanden_1979"
 #' RecType <- "Classic"
-#' Headerinfo <- read_iv_header_info_edb(Name, RecType, con)
+#' Headerinfo <- read_iv_header_info_edb(SurveyName, RecType, con)
 #' dbDisconnect(con)
 #' rm(con)
 
 
-read_iv_header_info_edb <- function(Name, RecType, .con) {
+read_iv_header_info_edb <- function(SurveyName, RecType, .con) {
   
-  assert_that(is.character(Name))
+  assert_that(is.character(SurveyName))
   assert_that(
     inherits(.con, "DBIConnection"),
     msg = "Er is geen connectie met de INBOVEG databank. Geef een connectie mee met de parameter .con" #nolint
@@ -49,9 +49,9 @@ read_iv_header_info_edb <- function(Name, RecType, .con) {
       INNER JOIN [dbo].[ivSurvey] ivS on ivS.Id = ivR.SurveyId
       INNER JOIN [dbo].[ivRecTypeD] ivRec on ivRec.ID = ivR.RecTypeID
       where ivR.NeedsWork = 0
-      AND ivS.Name LIKE {Name}
+      AND ivS.Name LIKE {SurveyName}
       AND ivREc.Name LIKE {RecType}",
-      ivS.Name = Name,
+      ivS.Name = SurveyName,
       ivRec.Name = RecType,
       .con = con))
   }
