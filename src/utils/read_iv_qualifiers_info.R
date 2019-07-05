f98da534b3e8838d6cc71e1507970a792b644abf
ElsDeBie <els.debie@inbo.be>
2019-07-05 14:15:01 +0200

    First trial with union, unsuccesfull till now. Trail and errors

---
+++ src/utils/read_iv_qualifiers_info.R
@@ -0,0 +1,238 @@
+library(tidyverse)
+library(DBI)
+library(glue)
+library(knitr)
+library(odbc)
+library(assertthat)
+library(inborutils)
+
+con <- connect_inbo_dbase("D0010_00_Cydonia")
+con_futon <- connect_inbo_dbase("D0013_00_Futon")
+
+## 3 aparte queries, achteraf mergen
+
+
+## QUERY00 - UNION
+(x <- c(sort(sample(1:20, 9)), NA))
+(y <- c(sort(sample(3:23, 7)), NA))
+union(x, y)
+
+order_by(10:1, cumsum(1:10))
+x <- 10:1
+y <- 1:10
+order_by(x, cumsum(y))
+
+
+?glue_sql
+
+
+tbl_ftQValues <- tbl(con_futon, from = "ftQualifierValues")
+
+  ftQValues_union <- tbl_ftQValues %>% 
+    select(Code, Description, ListGIVID) %>% 
+    order_by(ListGIVID) %>% 
+
+futon_List <- union(ftQualifierValues, ftDQualifierValues, ftAbiotiekValues )    
+    
+    
+    
+UnionActionGroups <- 
+dbGetQuery(con_futon, glue_sql("SELECT 
+      ftQValue.Code
+    , ftQValue.Description
+    , ftQValue.ListGIVID
+FROM tbl_ftQValues ftQValue
+ORDER BY  ftQValue.ListGIVID
+        , ftQValue.Code
+UNION ftDQualifierValues"))
+
+## uit prd-access van luc VH
+
+prd-query <- ("SELECT 
+      ftQValue.Code
+    , ftQValue.Description
+    , ftQValue.ListGIVID
+  FROM tbl_ftQValues ftQValue
+  ORDER BY  ftQValue.ListGIVID
+          , ftQValue.Code
+UNION(SELECT
+          ftDQValue.Code
+          , ftDQValue.Description
+          , ftDQValue.ListGIVID as
+      FROM ftDQualifierValues ftDQValue
+      ORDER BY  ftDQValue.ListGIVID
+              , ftDQValue.Code)
+UNION (SELECT
+          ftAbioV.Code
+          , ftAbioV.Description
+          , ftAbioV.ListGIVID
+       FROM ftAbiotiekValues ftAbioV
+       ORDER BY  ftAbioV.ListGIVID
+               , ftAbioV.Code)
+UNION (SELECT
+          ftBWKV.Code  
+          , ftBWKV.Description as oms
+          , ftBWKV.ListGIVID 
+      FROM D0013_00_Futon.dbo.ftBWKValues ftBWKV
+      ORDER BY  ftBWKV.ListGIVID
+              , ftBWKV.Code
+UNION (SELECT
+          ftCoverV.Code
+          , ftCoverV.PctValue as oms
+          , ftCoverV.ListGIVID
+       FROM D0013_00_Futon.dbo.ftCoverValues ftCoverV
+       ORDER BY  ftCoverV.ListGIVID
+               , ftCoverV.Code
+UNION (SELECT
+          ftFenoV.Code
+          , ftFenoV.Description as oms
+          , ftFenoV.ListGIVID
+       FROM D0013_00_Futon.dbo.ftFenoValues ftFenoV
+       ORDER BY  ftFenoV.ListGIVID
+                , ftFenoV.Code
+UNION (SELECT
+          ftGebiedV.Code
+          , ftGebiedV.Description as oms
+          , ftGebiedV.ListGIVID 
+      FROM D0013_00_Futon.dbo.ftgebiedValues ftGebiedV
+      ORDER BY  ftGebiedV.ListGIVID
+              ,ftGebiedV.Code)
+# UNION (SELECT
+#             ftGHCV.Code
+#             , ftGHCV."-" as oms
+#             , ftGHCV.ListGIVID
+#        FROM D0013_00_Futon.dbo.ftGHCValues ftGHCV
+#        ORDER BY  ftGHCV.ListGIVID
+#                , ftGHCV.Code)
+UNION (SELECT
+            ftLayerV.Code
+            , ftLayerV.Description as oms
+            , ftLayerV.ListGIVID 
+       FROM ftLayerValues ftLayerV
+       ORDER BY  ftLayerV.ListGIVID
+               , ftLayerV.Code)
+UNION (SELECT
+            ftLFV.Code
+            , ftLFV.Description as oms
+            , ftLFV.ListGIVID
+       FROM D0013_00_Futon.dbo.ftLFValues ftLFV
+       ORDER BY  ftLFV.ListGIVID
+               , ftLFV.Code
+UNION (SELECT
+            ftMngmtV.Code
+            , ftMngmtV.Description as oms
+            , ftMngmtV.ListGIVID 
+      FROM D0013_00_Futon.dbo.ftMngmtValues ftMngmtV
+      ORDER BY  ftMngmtV.ListGIVID, ftMngmtV.Code)
+UNION (SELECT
+            ftN2kV.Code
+            , ftN2kV.Description as oms
+            , ftN2kV.ListGIVID 
+      FROM D0013_00_Futon.dbo.ftN2kValues ftN2kV
+      ORDER BY  ftN2kV.ListGIVID
+              , ftN2kV.Code)
+UNION (SELECT
+            ftPatchV.Code
+            , ftPatchV.Description as oms
+            , ftPatchV.ListGIVID 
+      FROM D0013_00_Futon.dbo.ftPatchValues ftPatchV
+      ORDER BY  ftPatchV.ListGIVID
+              , ftPatchV.Code)
+UNION (SELECT
+            ftSociaV.Code
+            , ftSociaV.Description as oms
+            , ftSociaV.ListGIVID 
+      FROM D0013_00_Futon.dbo.ftSociaValues ftSociaV
+      ORDER BY  ftSociaV.ListGIVID
+              , ftSociaV.Code)
+ UNION (SELECT
+            ftSoilV.Code
+            , ftSoilV.Description as oms
+            , ftSoilV.ListGIVID
+        FROM D0013_00_Futon.dbo.ftSoilValues ftSoilV
+        ORDER BY  ftSoilV.ListGIVID
+            '    , ftSoilV.Code)
+UNION (SELECT
+            ftVitaV.Code
+            , ftVitaV.Description as oms
+            , ftVitaV.ListGIVID
+      FROM D0013_00_Futon.dbo.ftVitaValues ftVitaV
+      ORDER BY ftVitaV.ListGIVID
+             , ftVitaV.Code)")
+)
+              
+## functie opbouwen 
+
+inboveg_qualifiers <- function(connection,
+                                        survey_name,
+                                        collect = FALSE,
+                                        multiple = FALSE) {
+  
+  assert_that(inherits(connection, what = "Microsoft SQL Server"),
+              msg = "Not a connection object to database.")
+  
+  if (missing(survey_name) & !multiple) {
+    survey_name <- "%"
+  }
+  
+  if (missing(survey_name) & multiple) {
+    stop("Please provide one or more survey names to survey_name when multiple
+         = TRUE")
+  }
+  
+  if (!missing(survey_name)) {
+    if (!multiple) {
+      assert_that(is.character(survey_name))
+    } else {
+      assert_that(is.vector(survey_name, mode = "character"))
+    }
+  }
+  
+  common_part <- "SELECT 
+                      ivRecording.RecordingGivid
+                      , ivRecording.UserReference
+                      , ivRLHeadInfoD.MoistureCode
+                      , qry_01ACvalues_1.oms
+                      , ivRLHeadInfoD.GenHabCatCode
+                      , qry_01ACvalues.oms
+                  FROM (
+                   (ivRLHeadInfoD 
+            RIGHT JOIN ivRecording ON ivRLHeadInfoD.RecordingID = ivRecording.Id) 
+            LEFT JOIN qry_01ACvalues AS qry_01ACvalues_1 ON (ivRLHeadInfoD.MoistureCode = qry_01ACvalues_1.Code) 
+            AND (ivRLHeadInfoD.MoistureResource = qry_01ACvalues_1.ResourceGIVID)) 
+            LEFT JOIN qry_01ACvalues ON (ivRLHeadInfoD.GenHabCatCode = qry_01ACvalues.Code) 
+            AND (ivRLHeadInfoD.GenHabCatResource = qry_01ACvalues.ResourceGIVID)
+            ORDER BY ivRecording.RecordingGivid;"
+            
+ }
+
+
+ if (!multiple) {
+    sql_statement <- glue_sql(common_part,
+                              "AND ivS.Name LIKE {survey_name}",
+                              survey_name = survey_name,
+                              .con = connection)
+    
+  } else {
+    sql_statement <- glue_sql(common_part,
+                              "AND ivS.Name IN ({survey_name*})",
+                              survey_name = survey_name,
+                              .con = connection)
+  }
+  
+  query_result <- tbl(connection, sql(sql_statement))
+  
+  if (!isTRUE(collect)) {
+    return(query_result)
+  } else {
+    query_result <- collect(query_result)
+    return(query_result)
+  }
+  }
+
+## QUERY01 ACValues
+SELECT ivRLResources.ResourceGIVID, ivRLResources.ActionGroup, ivRLResources.ListName, ftActionGroupList.ListGIVID, qry_00ActionGroups.Code, qry_00ActionGroups.oms
+FROM (ivRLResources LEFT JOIN ftActionGroupList ON (ivRLResources.ListName = ftActionGroupList.ListName) AND (ivRLResources.ActionGroup = ftActionGroupList.ActionGroup)) LEFT JOIN qry_00ActionGroups ON ftActionGroupList.ListGIVID = qry_00ActionGroups.ListGIVID
+  ORDER BY ftActionGroupList.ListGIVID, qry_00ActionGroups.Code;
+  
+## Query03 Alles verbinden 
