aa0c8c01a2a1a7437d72c62806abbd668568a91a
ElsDeBie <els.debie@inbo.be>
2019-07-18 17:39:02 +0200

    just made a start, but not yet started really

---
+++ src/expert-classification/grassland_tryout.R
@@ -0,0 +1,5 @@
+# Gebaseerd op artikel Crosswalk of revised EUNIS habitat classification
+
+## enkel E34a en E34b bekijken (Moist or wet mesotrophic to eutrophic hay meadow/pasture)
+# a la calthions? # check dioagnostic species
+
